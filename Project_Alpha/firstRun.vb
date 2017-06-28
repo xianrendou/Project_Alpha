f1bd54463886decbfcc4c7c725e4168c5178bb48
Xiangtian Wei <weixiangtian@hotmail.com>
2017-06-28 23:15:57 +0800

    Add first run window into program.

---
+++ Project_Alpha/firstRun.vb
@@ -0,0 +1,9 @@
+﻿Public Class firstRun
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
